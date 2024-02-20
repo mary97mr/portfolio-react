e464123c3d7bdba3825632d4d2bad90674903795
Mari <52796151+mary97mr@users.noreply.github.com>
2024-02-20 18:05:44 +0100

    add missing packages and use Image component from next

--- app/components/transparent-card.tsx
+++ app/components/transparent-card.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image';
 
 const TransparentCard = ({title, iconPath, href}: {title: string, iconPath: string, href: string}) => {
   return (
     <Link href={href}>
       <div className="rounded-xl p-6 bg-white flex gap-x-4 shadow-lg bg-opacity-5 w-full hover:scale-105 md:hover:scale-110 ease-out duration-300 hover:cursor-pointer">
-        <img
+        <Image
+          alt={title}
           width={24}
           height={24}
           src={iconPath}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { Title } from './components/title'
 import { TransparentCard } from './components/transparent-card'
+import Image from 'next/image';
+
 
 export default function Page () {
   return (
     <div className='bg-zinc-800 h-screen flex flex-col justify-center'>
-      <img
-        className='mx-auto text-white mb-4'
+      <Image
+        alt="laptop code icon"
         src="/laptop-code.svg"
+        width={150}
+        height={150}
+        className='mx-auto text-white mb-4'
       />
       <Title fullName="Mari Mendieta "/>
       <section className='mt-8 gap-6 lg:gap-8 w-full max-w-2xl lg:max-w-4xl grid grid-cols-1 lg:grid-cols-3 sm:mx-auto p-3 md:p-0'>
